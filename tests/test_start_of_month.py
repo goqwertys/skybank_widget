6cef39ba3a0c84a53cfc39c5705ed55e5b31a23f
Georgii Ovechkin <goqwertys@gmail.com>
2024-07-23 02:41:17 +0400

    start_of_month() tests passed

---
+++ tests/test_start_of_month.py
@@ -0,0 +1,15 @@
+import pytest
+from datetime import datetime
+
+from src.utils import start_of_month
+
+
+@pytest.mark.parametrize("input_datetime, expected", [
+    (datetime(2024, 7, 22, 15, 30, 45), datetime(2024, 7, 1, 0, 0, 0)),
+    (datetime(2024, 12, 15, 8, 10, 20), datetime(2024, 12, 1, 0, 0, 0)),
+    (datetime(2024, 2, 1, 0, 0, 0), datetime(2024, 2, 1, 0, 0, 0)),
+    (datetime(2024, 8, 31, 23, 59, 59), datetime(2024, 8, 1, 0, 0, 0)),
+    (datetime(2024, 1, 1, 12, 00, 00), datetime(2024, 1, 1, 0, 0, 0))
+])
+def test_start_of_month(input_datetime, expected):
+    assert start_of_month(input_datetime) == expected
