0d786b8065cddd554caea84c05f6bbdf40dec147
Georgii Ovechkin <goqwertys@gmail.com>
2024-07-22 20:49:49 +0400

    greetings added

---
+++ src/config.py
@@ -0,0 +1,11 @@
+GREETINGS_DICT = {
+    "morning": "Доброе утро",
+    "afternoon": "Добрый день",
+    "evening": "Добрый вечер"
+}
+
+
+NIGHT_MORNING = '06:00:00'
+MORNING_AFTERNOON = '12:00:00'
+AFTERNOON_EVENING = '18:00:00'
+EVENING_NIGHT = '00:00:00'
---
+++ src/views.py
@@ -0,0 +1,30 @@
+"""Basic functions for generating JSON responses"""
+from datetime import datetime
+
+from src.config import GREETINGS_DICT
+from src.config import NIGHT_MORNING
+from src.config import MORNING_AFTERNOON
+from src.config import AFTERNOON_EVENING
+from src.config import EVENING_NIGHT
+
+
+def greetings(datetime_str: str) -> str:
+    """Returns greeting based on time"""
+    night_morning = datetime.strptime(NIGHT_MORNING, '%H:%M:%S').time()
+    morning_afternoon = datetime.strptime(MORNING_AFTERNOON, '%H:%M:%S').time()
+    afternoon_evening = datetime.strptime(AFTERNOON_EVENING, '%H:%M:%S').time()
+    evening_night = datetime.strptime(EVENING_NIGHT, '%H:%M:%S').time()
+
+    datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+    current_time = datetime_obj.time()
+
+    if night_morning <= current_time < morning_afternoon:
+        greeting = GREETINGS_DICT.get("morning")
+    elif afternoon_evening <= current_time < evening_night:
+        greeting = GREETINGS_DICT.get("afternoon")
+    elif evening_night <= current_time < night_morning:
+        greeting = GREETINGS_DICT.get("evening")
+    else:
+        greeting = GREETINGS_DICT.get("night")
+
+    return greeting
